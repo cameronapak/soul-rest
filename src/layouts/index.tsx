e3bf45281f2ad27eb1b8b5d94928f5585ae8461a
cameronapak <cameronandrewpak@gmail.com>
2025-12-22 16:47:57 -0600

    rm dark mode

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export const Layout: FC = ({ title = 'Home', children, head = null }: PropsWithChildren<Props>) => {
   return (
-    <html lang="en" class="dark">
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
